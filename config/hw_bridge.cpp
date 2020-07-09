c315d9889ef6de7851c255c45f49bde4aca4c073
younes <52610118+younesr1@users.noreply.github.com>
2020-07-09 18:04:56 -0400

    preparing for fw repo to use (#3)
    * clarity
    
    * add typedef to struct
    
    * trying to fix clang-format
    
    * still trying to fix clang-format
    
    * still working on ci
    
    * pls work
    
    Co-authored-by: yreda1999 <yreda@uwaterloo.ca>

--- config/hw_bridge.cpp
+++ config/hw_bridge.cpp
@@ -1,28 +1,41 @@
 #include "hw_bridge.h"
 
-std::string HW_BRIDGE::ARM::ACTUATOR::stringifyActuatorID(actuatorID_t actuator) {
-    switch(actuator) {
-        case(TURNTABLE): return "TURNTABLE";
-        case(SHOULDER): return"SHOULDER";
-        case(ELBOW): return "ELBOW";
-        case(WRISTLEFT): return "WRISTLEFT";
-        case(WRISTRIGHT): return "WRISTRIGHT";
-        case(CLAW) : return "CLAW";
-        default: return "ERROR";
-    }
+std::string HW_BRIDGE::ARM::ACTUATOR::stringifyActuatorID(HW_BRIDGE::ARM::ACTUATOR::actuatorID_t actuator) {
+  switch (actuator) {
+    case (HW_BRIDGE::ARM::ACTUATOR::TURNTABLE):
+      return "TURNTABLE";
+    case (HW_BRIDGE::ARM::ACTUATOR::SHOULDER):
+      return "SHOULDER";
+    case (HW_BRIDGE::ARM::ACTUATOR::ELBOW):
+      return "ELBOW";
+    case (HW_BRIDGE::ARM::ACTUATOR::WRISTLEFT):
+      return "WRISTLEFT";
+    case (HW_BRIDGE::ARM::ACTUATOR::WRISTRIGHT):
+      return "WRISTRIGHT";
+    case (HW_BRIDGE::ARM::ACTUATOR::CLAW):
+      return "CLAW";
+    default:
+      return "ERROR";
+  }
 }
 
-std::string HW_BRIDGE::ARM::PID::stringifyParam(parameter_t param) {
-    switch(param) {
-        case(P): return "P";
-        case(I): return "I";
-        case(D): return "D";
-        case(DEADZONE): return "DEADZONE";
-        case(BIAS): return "BIAS";
-        default: return "ERROR";
-    }
+std::string HW_BRIDGE::ARM::PID::stringifyParam(HW_BRIDGE::ARM::PID::parameter_t param) {
+  switch (param) {
+    case (HW_BRIDGE::ARM::PID::P):
+      return "P";
+    case (HW_BRIDGE::ARM::PID::I):
+      return "I";
+    case (HW_BRIDGE::ARM::PID::D):
+      return "D";
+    case (HW_BRIDGE::ARM::PID::DEADZONE):
+      return "DEADZONE";
+    case (HW_BRIDGE::ARM::PID::BIAS):
+      return "BIAS";
+    default:
+      return "ERROR";
+  }
 }
 
-std::string stringifyVelPos(bool vel_pos) {
-    return vel_pos ? "velocity" : "position";
+std::string HW_BRIDGE::ARM::PID::stringifyVelPos(bool vel_pos) {
+  return vel_pos ? "velocity" : "position";
 }
